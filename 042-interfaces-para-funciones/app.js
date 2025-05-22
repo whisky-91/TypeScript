f200188fbd69d516c141f7c8bba6c31c89fe74e5
Emi <sanchezesteban.emilia@gmail.com>
2025-05-22 09:27:03 +0200

    Add modificaciones 042?

--- 042-interfaces-para-funciones/app.js
+++ 042-interfaces-para-funciones/app.js
@@ -1,35 +1,6 @@
 "use strict";
 (() => {
-    console.log("Interfaces para las funciones:");
-    console.log("=============================");
-    ;
-    let suma;
-    suma = (a, b) => {
-        return a + b;
-    };
-    function Suma(a, b) {
-        return a + b;
-    }
-    ;
-    // let prueba: FuncionDosNumeros = Suma(0,9)
-    let sumar = function sumatory(a, b) {
-        return a + b;
-    };
-    let resp = sumar(3, 3);
-    console.log(resp);
-    console.log(typeof sumar);
-    //También se podría de forma indirecta a través de una variable
-    // objeto en lugar de una variable igualada a la función directamente:
-    const miObj = {
-        Suma(a, b) { return a + b; }
-    };
-    console.log(`Objeto con interfaz y función nombrada:`, miObj.Suma(3, 9));
-    console.log("Ejemplo JC:");
-    console.log("============");
-    ;
-    let dosNombres;
-    function saludar() {
-        return "holaaaa";
-    }
+    console.log("Namespace:");
+    console.log("===========");
 })();
 //# sourceMappingURL=app.js.map
